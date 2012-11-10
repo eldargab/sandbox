ca0b71ee49eceac92f48098f01426d47d2ca4228
Eldar Gabdullin <eldargab@gmail.com>
2012-11-10 19:40:57 +0400

    blobs

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,4 +2,4 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 cd ..
 P="$(pwd)"
 export PATH=$P/node_modules/.bin:$P/scripts:$PATH
-export NODE_PATH=$P/node_modules:$NODE_PATH
+export NODE_PATH=$P/lib:$NODE_PATH
